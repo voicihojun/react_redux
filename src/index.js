0c93ea8c300a292b083a9540f94f176a75d83c3f
Hojun Hwang <ho.jun.hwang@umontreal.ca>
2021-03-31 23:09:19 -0400

    redux module

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./modules";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
