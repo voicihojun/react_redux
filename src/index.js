c16ed0d1398dcd06cf5f3d40797038e9819893ae
Hojun Hwang <ho.jun.hwang@umontreal.ca>
2021-04-03 17:27:19 -0400

    add redux-devtools-extension, composeWithDevTools

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./modules";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
